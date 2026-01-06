2e50098303ff3d2d8fc1534b4648d41696239bf1
pastorelana45-beep <pastorelana45@gmail.com>
2026-01-06 17:49:19 +0100

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,41 @@
+
+export interface Instrument {
+  id: string;
+  name: string;
+  category: string;
+  icon?: string;
+}
+
+export enum WorkstationMode {
+  IDLE = 'IDLE',
+  LIVE = 'LIVE',
+  RECORD = 'RECORD'
+}
+
+export interface RecordedNote {
+  note: string;
+  time: number;
+  duration: number;
+}
+
+export interface StudioSession {
+  id: string;
+  timestamp: number;
+  midiNotes: RecordedNote[];
+  audioUrl: string;
+  instrumentName: string;
+}
+
+export enum Category {
+  ALL = 'ALL',
+  PIANO = 'PIANO',
+  KEYS = 'KEYS',
+  PERC = 'PERC',
+  ORGAN = 'ORGAN',
+  GUITAR = 'GUITAR',
+  BASS = 'BASS',
+  STRINGS = 'STRINGS',
+  BRASS = 'BRASS',
+  REED = 'REED',
+  SYNTH = 'SYNTH'
+}
